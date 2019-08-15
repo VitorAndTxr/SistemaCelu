d2d5b78c0c7901bfbc2cf9b52596f33d4606dc7e
Vitor Teixeira <vgabrielteixeira@gmail.com>
2019-08-15 16:02:54 -0300

    Inicio do MVC

---
+++ WebApplication1/WebApplication1/Models/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Class1
+    {
+        private Guid id { get; set; }
+        private string name { get; set; }
+        private string docNumber { get; set; }
+        private int docType { get; set; }
+        private DateTime birthDate { get; set; }
+        private int country { get; set; }
+
+    }
+}
